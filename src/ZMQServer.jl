09e31c8b02ffc1518609d85f56b21f68e08aae91
rejuvyesh <mail@rejuvyesh.com>
2017-08-08 10:04:03 -0700

    some comments

--- src/ZMQServer.jl
+++ src/ZMQServer.jl
@@ -25,48 +25,60 @@ function ZMQTransport(ip, port, mode::Int, bound::Bool, ctx::Context=Context())
     ZMQTransport("tcp://$ip:$port", mode, bound, ctx)
 end
 
+"""Close connection to the socket"""
 function close(conn::ZMQTransport)
     Logging.debug("closing connection...")
     close(conn.sock)
 end
 
+"""Receive request from the socket"""
 function recvreq(conn::ZMQTransport)
     reqstr = unsafe_string(ZMQ.recv(conn.sock))
     Logging.debug("received request: ", reqstr)
     reqstr
 end
 
+"""Send response to the socket"""
 function sendresp(conn::ZMQTransport, msgstr)
     Logging.debug("sending response: ", msgstr)
     ZMQ.send(conn.sock, JSON.json(msgstr))
 end
 
-function run_env_server(ip, port, prob)
-    env = MDPEnvironment(prob)
+function process(msg::Dict{String, Any})
+    if "cmd" in keys(msg)
+        if msg["cmd"] == "obs_dimensions"
+            respmsg = Dict("obs_dim"=>obs_dimensions(env))
+        elseif msg["cmd"] == "n_actions"
+            respmsg = Dict("n_actions"=>n_actions(env))
+        elseif msg["cmd"] == "render"
+            render(env)
+        elseif msg["cmd"] == "reset"
+            obs = reset(env)
+            respmsg = Dict("obs"=> obs)
+        elseif msg["cmd"] == "step"
+            act_idx = msg["args"]
+            act = actions(env)[act_idx]
+            obs, rew, done, info = step!(env, act)
+            respmsg = Dict("obs"=> obs, "rew"=> rew, "done"=>done, "info"=>info)
+        else
+            respmsg = Dict("error"=>"no known "+msg["cmd"]+" cmd found")
+        end
+    end
+    respmsg
+end
+
+function run_env_server(ip, port, prob; pomdp=false)
+    if pomdp
+        env = POMDPEnvironment(prob)
+    else
+        env = MDPEnvironment(prob)
+    end
     conn = ZMQTransport(ip, port, ZMQ.REP, true)
     Logging.debug("running server...")
     while true
         msg = JSON.parse(recvreq(conn))
         Logging.info("received request: ", msg)
-        if "cmd" in keys(msg)
-            if msg["cmd"] == "obs_dimensions"
-                respmsg = Dict("obs_dim"=>obs_dimensions(env))
-            elseif msg["cmd"] == "n_actions"
-                respmsg = Dict("n_actions"=>n_actions(env))
-            elseif msg["cmd"] == "render"
-                render(env)
-            elseif msg["cmd"] == "reset"
-                obs = reset(env)
-                respmsg = Dict("obs"=> obs)
-            elseif msg["cmd"] == "step"
-                act_idx = msg["args"]
-                act = actions(env)[act_idx]
-                obs, rew, done, info = step!(env, act)
-                respmsg = Dict("obs"=> obs, "rew"=> rew, "done"=>done, "info"=>info)
-            else
-                respmsg = Dict("error"=>"no known "+msg["cmd"]+" cmd found")
-            end
-        end
+        respmsg = process(msg)
         sendresp(conn, respmsg)
     end
     close(conn)
