13ceba0dd254443d44d4667174043d680a69eec3
MaximeBouton <maxim.bouton@gmail.com>
2017-10-06 18:31:21 -0700

    fixed call to convert_o

--- src/DeepRL.jl
+++ src/DeepRL.jl
@@ -13,7 +13,7 @@ using JSON
 import Logging
 
 
-export 
+export
     # Environment types
     AbstractEnvironment,
     POMDPEnvironment,
@@ -31,7 +31,7 @@ export
 abstract type AbstractEnvironment end
 
 mutable struct MDPEnvironment{S} <: AbstractEnvironment
-    problem::MDP 
+    problem::MDP
     state::S
     rng::AbstractRNG
 end
@@ -40,7 +40,7 @@ function MDPEnvironment(problem::MDP; rng::AbstractRNG=MersenneTwister(0))
 end
 
 mutable struct POMDPEnvironment{S} <: AbstractEnvironment
-    problem::POMDP 
+    problem::POMDP
     state::S
     rng::AbstractRNG
 end
@@ -55,12 +55,12 @@ Reset an MDP environment by sampling an initial state returning it.
 function Base.reset(env::MDPEnvironment)
     s = initial_state(env.problem, env.rng)
     env.state = s
-    return convert_s(Array{Float64}, s, env.problem)
+    return convert_s(Array{Float64, 1}, s, env.problem)
 end
 
 """
     reset(env::POMDPEnvironment)
-Reset an POMDP environment by sampling an initial state, 
+Reset an POMDP environment by sampling an initial state,
 generating an observation and returning it.
 """
 function Base.reset(env::POMDPEnvironment)
@@ -73,8 +73,8 @@ end
 
 """
     step!{A}(env::POMDPEnvironment, a::A)
-Take in an POMDP environment, and an action to execute, and 
-step the environment forward. Return the state, reward, 
+Take in an POMDP environment, and an action to execute, and
+step the environment forward. Return the state, reward,
 terminal flag and info
 """
 function step!(env::MDPEnvironment, a::A) where A
@@ -82,14 +82,14 @@ function step!(env::MDPEnvironment, a::A) where A
     env.state = s
     t = isterminal(env.problem, s)
     info = nothing
-    obs = convert_s(Array{Float64}, s, env.problem)
+    obs = convert_s(Array{Float64, 1}, s, env.problem)
     return obs, r, t, info
 end
 
 """
     step!{A}(env::MDPEnvironment, a::A)
-Take in an MDP environment, and an action to execute, and 
-step the environment forward. Return the observation, reward, 
+Take in an MDP environment, and an action to execute, and
+step the environment forward. Return the observation, reward,
 terminal flag and info
 """
 function step!(env::POMDPEnvironment, a::A) where A
@@ -97,7 +97,7 @@ function step!(env::POMDPEnvironment, a::A) where A
     env.state = s
     t = isterminal(env.problem, s)
     info = nothing
-    obs = convert_o(Array{Float64}, o, env.problem)
+    obs = convert_o(Array{Float64, 1}, o, env.problem)
     return obs, r, t, info
 end
 
@@ -127,12 +127,12 @@ end
 
 
 function obs_dimensions(env::MDPEnvironment)
-    return size(convert_s(Array{Float64}, initial_state(env.problem, env.rng), env.problem))
+    return size(convert_s(Array{Float64,1}, initial_state(env.problem, env.rng), env.problem))
 end
 
 
 function obs_dimensions(env::POMDPEnvironment)
-    return size(convert_o(Array{Float64}, generate_o(env.problem, initial_state(env.problem, env.rng), env.rng), env.problem))
+    return size(convert_o(Array{Float64,1}, generate_o(env.problem, initial_state(env.problem, env.rng), env.rng), env.problem))
 end
 
 """
